8af7f933c41b8687e97942eed5dd20df730b4ded
Jeiel Libert <jeiellibert@gmail.com>
2014-10-09 05:12:34 +0000

    changed SQL; added transaction and batch insert statements

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -26,26 +26,32 @@ angular.module('myApp.controllers', []).
         var hero_count = data.heroes.length;
         var sql = new Object();
         sql.autoIncr = data.heroes.length+1;
-        sql.insertHeroes = [];
-        sql.insertHeroStats = [];
+        sql.insertHeroes = '';
+        sql.insertHeroStats = '';
 
         for(var i=0;i<data.heroes.length;i++){ // Loop through hero data and create sql statement fragments
            HeroStatsService.getHeroStats(data.heroes[i]).
            then(function(data) {
              
-             // Append to array insert statement fragments
-             sql.insertHeroes.push('("'+data.hero.id+'","'+addslashes(data.hero.name)+'","'+addslashes(data.hero.type)+'", "'+addslashes(data.hero.icon)+'",'+(completed_requests+1)+')');
-             sql.insertHeroStats.push('('+(completed_requests+1)+', '+data.hero.stats[0]+', "'+data.hero.stats[1]+'", '+data.hero.stats[2]+', '+data.hero.stats[3]+', "'+data.hero.stats[5]+'", "'+data.hero.stats[6]+'", "'+data.hero.stats[7]+'", '+data.hero.stats[9]+', '+data.hero.stats[8]+', '+data.hero.stats[10]+')');
-             
+             // Append to array SQL fragments
+             sql.insertHeroes += '("'+data.hero.id+'","'+addslashes(data.hero.name)+'","'+addslashes(data.hero.type)+'", "'+addslashes(data.hero.icon)+'",'+(completed_requests+1)+'), ';
+             sql.insertHeroStats += '('+(completed_requests+1)+', '+data.hero.stats[0]+', "'+data.hero.stats[1]+'", '+data.hero.stats[2]+', '+data.hero.stats[3]+', "'+
+                                     data.hero.stats[5]+'", "'+data.hero.stats[6]+'", "'+data.hero.stats[7]+'", '+data.hero.stats[9]+', '+data.hero.stats[8]+', '+data.hero.stats[10]+'), ';
+
              completed_requests++;
              progress_bar=Math.ceil((completed_requests/hero_count)*90); // Calculate the progress % of requests needed to generate sql, reserve 10% for executing SQL
              $scope.progress = progress_bar;
              $scope.progress_message = 'Generating '+addslashes(data.hero.name)+'...'; // Update progress message for each hero generated
 
              if(completed_requests == hero_count){
-               /* Run Generated SQL */
-               $scope.progress_message = 'Creating and Populating Database...';
-               HonDBService.createDB(sql).then(function(data){
+               $scope.progress_message = 'Creating and Populating Database...'; // Update progress message for database activity
+               
+               // Remove trailing ',' from SQL fragments
+               sql.insertHeroes = sql.insertHeroes.substring(0, sql.insertHeroes.length - 2);
+               sql.insertHeroStats = sql.insertHeroStats.substring(0, sql.insertHeroStats.length - 2);
+               
+               // Call database service
+               HonDBService.createDB(sql).then(function(){
                  $scope.progress=100; // Assign 100% when DB update complete (remaining 10%)
                  $scope.progress_message = 'Completed!';
                });
--- routes/api.js
+++ routes/api.js
@@ -109,29 +109,47 @@ router.post('/createDB', function(req, res, next){
       connection.query('USE `honDB`;',[],function(err){
         if (err) return next(err);
         connection.query('CREATE TABLE IF NOT EXISTS `heroes` (`id` varchar(36) NOT NULL, `name` varchar(40) NOT NULL, `herotype` varchar(20) NOT NULL,`icon` varchar(150) NOT NULL, `statsId` int(11) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `name` (`name`), KEY `statsId` (`statsId`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;',[],function(err){
-          if (err) return next(err);
-          connection.query('DROP TABLE IF EXISTS `herostats`;',[],function(err){
-            if (err) return next(err);
-            connection.query('CREATE TABLE IF NOT EXISTS `herostats` (`id` int(11) NOT NULL AUTO_INCREMENT, `health` int(11) NOT NULL, `damage` varchar(6) NOT NULL,'+
+        if (err) return next(err);
+        connection.query('CREATE TABLE IF NOT EXISTS `herostats` (`id` int(11) NOT NULL AUTO_INCREMENT, `health` int(11) NOT NULL, `damage` varchar(6) NOT NULL,'+
                   '`mana` decimal(6,2) NOT NULL, `armor` decimal(6,2) NOT NULL, `strength` varchar(15) NOT NULL, `intelligence` varchar(15) NOT NULL, `agility` varchar(15) NOT NULL,'+
                   '`attackspeed` decimal(2,2) NOT NULL, `atkrange` int(11) NOT NULL, `movementspeed` int(11) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1'+
                   ' AUTO_INCREMENT='+req.body.sql.autoIncr+';',[],function(err){
-              if (err) return next(err);
-              for(var i=0;i<req.body.sql.insertHeroes.length;i++){ // loop through insertHeroes and execute insert statement
-                connection.query('INSERT INTO `heroes` (`id`, `name`, `herotype`, `icon`, `statsId`) VALUES '+req.body.sql.insertHeroes[i]+' ON DUPLICATE KEY UPDATE `name`=VALUES(name), `herotype`=VALUES(herotype), `icon`=VALUES(icon);',[],function(err){
-                  if (err) return next(err); //roll back on failure -- requires transaction
+            if (err) return next(err);
+            
+              // Begin Transactions
+              connection.beginTransaction(function(err){
+                if (err) return next(err);
+                
+                connection.query('INSERT INTO `herostats` (`id`, `health`, `damage`, `mana`, `armor`, `strength`, `intelligence`, `agility`, `attackspeed`, `atkrange`, `movementspeed`) VALUES '+req.body.sql.insertHeroStats+' ON DUPLICATE KEY UPDATE `health`=VALUES(health), `damage`=VALUES(damage), `mana`=VALUES(mana), `armor`=VALUES(armor), `strength`=VALUES(strength), `intelligence`=VALUES(intelligence), `agility`=VALUES(agility), `attackspeed`=VALUES(attackspeed), `atkrange`=VALUES(atkrange), `movementspeed`=VALUES(movementspeed);',[],function(err){
+                  if (err){
+                    connection.rollback(function(){ return next(err); });
+                  }
                 });
-              }
-              for(i=0;i<req.body.sql.insertHeroStats.length;i++){ // loop through insertHeroStats and execute insert statement
-                connection.query('INSERT INTO `herostats` (`id`, `health`, `damage`, `mana`, `armor`, `strength`, `intelligence`, `agility`, `attackspeed`, `atkrange`, `movementspeed`) VALUES '+req.body.sql.insertHeroStats[i]+' ON DUPLICATE KEY UPDATE `health`=VALUES(health), `damage`=VALUES(damage), `mana`=VALUES(mana), `armor`=VALUES(armor), `strength`=VALUES(strength), `intelligence`=VALUES(intelligence), `agility`=VALUES(agility), `attackspeed`=VALUES(attackspeed), `atkrange`=VALUES(atkrange), `movementspeed`=VALUES(movementspeed);',[],function(err){
-                  if (err) return next(err); //roll back on failure -- requires transaction
+                
+                connection.query('INSERT INTO `heroes` (`id`, `name`, `herotype`, `icon`, `statsId`) VALUES '+req.body.sql.insertHeroes+' ON DUPLICATE KEY UPDATE `name`=VALUES(name), `herotype`=VALUES(herotype), `icon`=VALUES(icon);',[],function(err){
+                  if (err){
+                    connection.rollback(function(){ return next(err); });
+                  }
+                  connection.commit(function(err) {
+                    if (err) { 
+                      connection.rollback(function(){ return next(err); });
+                    }
+                    console.log('success!');
+                  });
                 });
-              }
-              // Add constraint
-              connection.query('ALTER TABLE `heroes` ADD CONSTRAINT `heroes_ibfk_1` FOREIGN KEY (`statsId`) REFERENCES `herostats` (`id`);',[],function(err){
-                if (err) return next(err); //roll back on failure -- requires transaction
-                res.json({success: 'success'});
+                
               });
+            // Add constraint if it doesn't exist
+            connection.query('SELECT IF(EXISTS(SELECT * FROM information_schema.TABLE_CONSTRAINTS WHERE CONSTRAINT_SCHEMA = "honDB" AND CONSTRAINT_NAME = "heroes_ibfk_1" AND CONSTRAINT_TYPE = "FOREIGN KEY"), 1, 0) AS RESULT;', [], function(err, rows){
+              if (err) return next(err);
+              if(rows[0].RESULT === 0){
+                connection.query('ALTER TABLE `heroes` ADD CONSTRAINT `heroes_ibfk_1` FOREIGN KEY (`statsId`) REFERENCES `herostats` (`id`);',[],function(err){
+                  if (err) return next(err);
+                  res.json({success: 'success'});
+                });  
+              }else{
+                res.json({success: 'success'});
+              }
             });
           });
         });
